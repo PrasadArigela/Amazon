661968c2459665fb5590a851ba9d0d548dbc59a2
PrasadArigela <88876018+PrasadArigela@users.noreply.github.com>
2022-04-25 14:35:22 +0530

    Update ascii.py

--- ascii.py
+++ ascii.py
@@ -4,3 +4,11 @@ textlenght = len(text)
 for char in text:
    ascii = ord(char)
    print(char," ",ascii)
+   
+ 
+i = 10
+if i % 2 == 0:
+   print("The Number is Even Number:")
+else:
+   print("The Number is Odd Number:")
+   
